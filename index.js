3cf065d070a288d0759b73c06ffa834c7fac2a4b
Jayedra Matarage <jayendra@bhasha.lk>
2022-07-01 08:13:58 +0530

    port changed

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/send-to', (req, res) => {
 io.on('connection', (socket) => {
     console.log('a user connected');
 });
-
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
     console.log('listening on *:3000');
 });
