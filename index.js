12b11cb4c8907b1586ceba1b84a6bc19a16bd29d
Jayedra Matarage <jayendra@bhasha.lk>
2022-07-01 08:15:26 +0530

    send to route added to emmit

--- index.js
+++ index.js
@@ -21,9 +21,8 @@ app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
 app.use(bodyParser.raw()); // support encoded bodies
 
 app.post('/send-to', (req, res) => {
-    //const {id, content} = req.body;
-    //const emitted = io.emit(id, content);
-    const emitted = true;
+    const {id, content} = req.body;
+    const emitted = io.emit(id, content);
     let code = emitted ? 200 : 500;
     let msg = emitted ? "Successfully emitted" : "Error while emitting";
     return res.send({
